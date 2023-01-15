1310581e382d559b04aac362ec42a2b06363d630
Rust <madishkin2@gmail.com>
2023-01-15 11:56:05 +0300

    commit1

---
+++ src/Lesson/Lesson18.java
@@ -0,0 +1,11 @@
+package Lesson;
+
+public class Lesson18 {
+    public static void main (String[] args){
+        int a = 5;
+        System.out.println(5>7 ? 8 : 9);
+        System.out.println ("Hello World");
+    }
+
+
+}
